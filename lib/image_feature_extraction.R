c2f30c9d26adcfeab360b3ebeb55f8c4597765a7
XuehanLiu <xl2615@columbia.edu>
2017-04-26 12:47:04 -0400

    main+image feature

--- lib/image_feature_extraction.R
+++ lib/image_feature_extraction.R
@@ -1,23 +1,18 @@
----
 title: "Image Anlysis"
 author: "Xuehan Liu (xl2615)"
 date: "4/21/2017"
 output: pdf_document
----
 
-###Load libraries
-```{r}
 library(EBImage)
 #install.packages("jpeg")
 library(jpeg)
 library(OpenImageR)
-```
+
 
 
 ###Extract HoG Features from images
 
-```{r}
-setwd("/Users/xuehan/Desktop/Spr2017-proj5-grp8/doc")
+
 source("../lib/HOGFeature.R")
 dir <- "../data/"
 img_dir_full <- paste(dir, "Images_full/", sep="")
@@ -25,23 +20,22 @@ h<-hogfeature(img_dir_full,export=T)
 h<-data.frame(h)
 
 file.names <- list.files(img_dir_full,pattern="*.jpg") 
-file.names
+#file.names
 
 file.names.short<-NULL
 for (i in 1:length(file.names)){
-file.names.short[i]<-substr(file.names[[i]],1,nchar(file.names[[1]])-4)
+  file.names.short[i]<-substr(file.names[[i]],1,nchar(file.names[[i]])-4)
 }
 
 h$id<-file.names.short
 write.csv(h,file="HoG.csv")
 #HoG.csv is under the doc 
 
-```
+
 
 
 ###Extract RGB Features from images 
 
-```{r}
 source("../lib/RGBfeature.R")
 dir <- "../data/"
 img_dir_full <- paste(dir, "Images_full", sep="")
@@ -53,6 +47,6 @@ rgb<-data.frame(rgb)
 rgb$id<-file.names.short
 
 write.csv(rgb,file="RGB.csv") #stored under doc
-```
+
 
 
